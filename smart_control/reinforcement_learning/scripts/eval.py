51992ff63ae1cd63c4a4dfadeca48c9bd13cf72d
Michael Rossetti <mjrossetti@google.com>
2025-08-15 17:20:04 +0000

    WIP - reproducing eval script - encounter env config errors

--- smart_control/reinforcement_learning/scripts/eval.py
+++ smart_control/reinforcement_learning/scripts/eval.py
@@ -3,13 +3,15 @@ Script to evaluate a trained reinforcement learning policy.
 This script loads a saved policy and evaluates it on a configured environment.
 """
 
-import argparse
-from datetime import datetime
+# from datetime import datetime
 import logging
 import os
 import shutil
 import tempfile
+from typing import Sequence
 
+from absl import app
+from absl import flags
 import tensorflow as tf
 from tf_agents.environments import tf_py_environment
 from tf_agents.metrics import tf_metrics
@@ -21,16 +23,54 @@ from smart_control.reinforcement_learning.observers.print_status_observer import
 from smart_control.reinforcement_learning.observers.trajectory_recorder_observer import TrajectoryRecorderObserver
 from smart_control.reinforcement_learning.policies.saved_model_policy import SavedModelPolicy
 from smart_control.reinforcement_learning.policies.schedule_policy import create_baseline_schedule_policy
+from smart_control.reinforcement_learning.utils.constants import ONE_DAY_CONFIG_FILEPATH
+from smart_control.reinforcement_learning.utils.constants import RL_EXPERIMENT_EVAL_DIR
 from smart_control.reinforcement_learning.utils.constants import RL_EXPERIMENT_RESULTS_DIR
 from smart_control.reinforcement_learning.utils.environment import create_and_setup_environment
 from smart_control.utils.constants import ROOT_DIR
-from smart_control.utils.constants import SB1_GIN_CONFIG_FILEPATH
+
+# from smart_control.utils.constants import SB1_GIN_CONFIG_FILEPATH
+
+# LOGGING
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] [%(filename)s:%(lineno)d] [%(message)s]",
 )
 logger = logging.getLogger(__name__)
+
+# FLAGS
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    name="eval_experiment_name",
+    default=None,
+    help="Name of the evaluation experiment",
+    # required=True,
+)
+
+flags.DEFINE_string(
+    name="eval_policy_dirpath",
+    default=None,
+    help=(
+        "Path to the directory containing the saved policy. To use schedule"
+        " policy, use: 'schedule'"
+    ),
+    # required=True,
+)
+
+flags.DEFINE_string(
+    name="eval_config_filepath",
+    default=ONE_DAY_CONFIG_FILEPATH,  # SB1_GIN_CONFIG_FILEPATH,
+    help="Path to the .gin config file",
+)
+
+flags.DEFINE_integer(
+    name="num_eval_episodes",
+    default=1,
+    help="Number of episodes for evaluation",
+)
 
 
 def find_latest_checkpoint(policy_dir):
@@ -123,34 +163,40 @@ def create_merged_saved_model(policy_dir):
 
 
 def evaluate_policy(
-    policy_dir,
-    gin_config_path,
-    experiment_name,
-    num_eval_episodes=10,
-    save_trajectory=True,
+    experiment_name: str,
+    config_filepath: str,
+    policy_dirpath: str = None,
+    num_eval_episodes: int = 10,
+    save_trajectory: bool = True,
 ):
   """
   Evaluates a trained policy on a configured environment.
 
   Args:
-      policy_dir: Path to the directory containing the saved policy
-      gin_config_path: Path to the .gin config file
-      experiment_name: Name of the evaluation experiment
+      experiment_name: Name of the experiment to evaluate. Corresponds with an
+        existing directory in the "experiment_results" directory.
+      policy_dirpath: Path to the directory containing the saved policy or
+        "schedule".
+      config_filepath: Path to the .gin config file
       num_eval_episodes: Number of episodes to evaluate
       save_trajectory: Whether to save detailed trajectory data for each episode
   """
   # Get base directory for evaluation results
-  base_dir = os.path.dirname(RL_EXPERIMENT_RESULTS_DIR)
-  eval_results_path = os.path.join(base_dir, "eval_results")
-  os.makedirs(eval_results_path, exist_ok=True)
+  # base_dir = os.path.dirname(RL_EXPERIMENT_RESULTS_DIR)
+  # eval_results_path = os.path.join(base_dir, "experiment_eval")
+  # eval_results_path = os.path.join(RL_EXPERIMENT_RESULTS_DIR,
+  # "experiment_eval")
+  # os.makedirs(eval_results_path, exist_ok=True)
+  os.makedirs(RL_EXPERIMENT_EVAL_DIR, exist_ok=True)
 
-  # Generate timestamp for results directory
-  current_time = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
-  results_dir = os.path.join(
-      eval_results_path, f"{experiment_name}_{current_time}"
-  )
+  # results directory
+  # current_time = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+  # results_dir = os.path.join(
+  #    eval_results_path, f"{experiment_name}_{current_time}"
+  # )
+  experiment_dirname = experiment_name.replace(" ", "")
+  results_dir = os.path.join(RL_EXPERIMENT_EVAL_DIR, experiment_dirname)
   logger.info("Evaluation results will be saved to %s", results_dir)
-
   try:
     os.makedirs(results_dir, exist_ok=False)
   except FileExistsError as exc:
@@ -159,6 +205,8 @@ def evaluate_policy(
         f"Directory {results_dir} already exists. Exiting."
     ) from exc
 
+  # ENV
+
   # Create metrics directory
   metrics_dir = os.path.join(results_dir, "metrics")
   os.makedirs(metrics_dir, exist_ok=True)
@@ -166,7 +214,7 @@ def evaluate_policy(
   # Create eval environment
   logger.info("Creating evaluation environment")
   eval_env = create_and_setup_environment(
-      gin_config_path, metrics_path=metrics_dir
+      gin_config_file=config_filepath, metrics_path=metrics_dir
   )
 
   # Wrap in TF environment
@@ -176,39 +224,37 @@ def evaluate_policy(
   eval_step = tf.Variable(0, trainable=False, dtype=tf.int64)
 
   # Create policy based on the type
-  temp_dir = None
+  temp_policy_dirpath = None
   try:
-    if policy_dir == "schedule":
+    if policy_dirpath == "schedule":
       logger.info("Using schedule policy")
       policy = create_baseline_schedule_policy(eval_tf_env)
     else:
+      experiment_results_dirpath = os.path.join(
+          RL_EXPERIMENT_RESULTS_DIR, experiment_dirname
+      )
+      policy_dirpath = os.path.join(experiment_results_dirpath, "policies")
+
       # Create a merged saved model with structure from policy dir and variables
       # from latest checkpoint
-      temp_dir = create_merged_saved_model(policy_dir)
+      temp_policy_dirpath = create_merged_saved_model(policy_dirpath)
 
       # Use SavedModelPolicy for saved model
-      logger.info("Loading saved model from %s", temp_dir)
+      logger.info("Loading saved model from %s", temp_policy_dirpath)
       policy = SavedModelPolicy(
-          temp_dir, eval_tf_env.time_step_spec(), eval_tf_env.action_spec()
+          saved_model_path=temp_policy_dirpath,
+          time_step_spec=eval_tf_env.time_step_spec(),
+          action_spec=eval_tf_env.action_spec(),
       )
       logger.info("Saved model policy created")
 
-    # Set up metrics
-    eval_metrics = [
-        tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
-        tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes),
-        tf_metrics.MaxReturnMetric(buffer_size=num_eval_episodes),
-        tf_metrics.MinReturnMetric(buffer_size=num_eval_episodes),
-        tf_metrics.NumberOfEpisodes(),
-        tf_metrics.EnvironmentSteps(),
-    ]
+    # OBSERVERS
 
     observers_list = []
 
     print_observer = PrintStatusObserver(
         status_interval_steps=1, environment=eval_tf_env, replay_buffer=None
     )
-
     observers_list.append(print_observer)
 
     # Record trajectory observer
@@ -225,23 +271,36 @@ def evaluate_policy(
 
     observers = CompositeObserver(observers_list)
 
+    # ACTOR
+
     # Create eval actor with observers
     logger.info("Creating evaluation actor")
+    eval_dirpath = os.path.join(results_dir, "eval")
     eval_actor = actor.Actor(
-        eval_env,
-        py_tf_eager_policy.PyTFEagerPolicy(policy),
-        eval_step,
+        env=eval_env,
+        policy=py_tf_eager_policy.PyTFEagerPolicy(policy),
+        train_step=eval_step,
         episodes_per_run=num_eval_episodes,
         metrics=actor.eval_metrics(num_eval_episodes),
         observers=[observers],
-        summary_dir=os.path.join(results_dir, "eval"),
+        summary_dir=eval_dirpath,
         summary_interval=1,
     )
+
+    # EVAL
 
     # Run evaluation
     logger.info("Starting evaluation for %d episodes", num_eval_episodes)
     eval_actor.run()
 
+    eval_metrics = [
+        tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
+        tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes),
+        tf_metrics.MaxReturnMetric(buffer_size=num_eval_episodes),
+        tf_metrics.MinReturnMetric(buffer_size=num_eval_episodes),
+        tf_metrics.NumberOfEpisodes(),
+        tf_metrics.EnvironmentSteps(),
+    ]
     # Write evaluation summaries
     with eval_actor.summary_writer.as_default():
       for m in eval_metrics:
@@ -254,57 +313,36 @@ def evaluate_policy(
 
   finally:
     # Clean up temporary directory if created
-    if temp_dir and os.path.exists(temp_dir):
-      logger.info("Cleaning up temporary directory: %s", temp_dir)
-      shutil.rmtree(temp_dir)
+    if temp_policy_dirpath and os.path.exists(temp_policy_dirpath):
+      logger.info("Cleaning up temporary directory: %s", temp_policy_dirpath)
+      shutil.rmtree(temp_policy_dirpath)
+
+
+def main(argv: Sequence[str]):
+  if len(argv) > 1:
+    raise app.UsageError("Too many command-line arguments.")
+
+  # handle relative and absolute filepaths:
+  config_filepath = FLAGS.eval_config_filepath
+  if not os.path.isabs(config_filepath):
+    config_filepath = os.path.join(ROOT_DIR, config_filepath)
+
+  policy_dirpath = FLAGS.eval_policy_dirpath
+  if (
+      policy_dirpath is not None
+      and not os.path.isabs(policy_dirpath)
+      and policy_dirpath != "schedule"
+  ):
+    policy_dirpath = os.path.join(ROOT_DIR, policy_dirpath)
+
+  evaluate_policy(
+      experiment_name=FLAGS.eval_experiment_name,
+      policy_dirpath=policy_dirpath,
+      config_filepath=config_filepath,
+      num_eval_episodes=FLAGS.num_eval_episodes,
+  )
 
 
 if __name__ == "__main__":
 
-  parser = argparse.ArgumentParser(
-      description="Evaluate a trained reinforcement learning policy"
-  )
-  parser.add_argument(
-      "--policy-dir",
-      type=str,
-      required=True,
-      help=(
-          "Path to the directory containing the saved policy. To use schedule"
-          " policy, just type `schedule`"
-      ),
-  )
-  parser.add_argument(
-      "--gin-config",
-      type=str,
-      default=SB1_GIN_CONFIG_FILEPATH,
-      help="Path to the .gin config file",
-  )
-  parser.add_argument(
-      "--num-eval-episodes",
-      type=int,
-      default=1,
-      help="Number of episodes for evaluation",
-  )
-  parser.add_argument(
-      "--experiment-name",
-      type=str,
-      required=True,
-      help="Name of the evaluation experiment",
-  )
-
-  args = parser.parse_args()
-
-  # Make it work for both relative and absolute paths
-  gin_config_path_ = args.gin_config
-  if not os.path.isabs(args.gin_config):
-    gin_config_path_ = os.path.join(ROOT_DIR, args.gin_config)
-
-  if not os.path.isabs(args.policy_dir) and args.policy_dir != "schedule":
-    args.policy_dir = os.path.join(ROOT_DIR, args.policy_dir)
-
-  evaluate_policy(
-      policy_dir=args.policy_dir,
-      gin_config_path=gin_config_path_,
-      experiment_name=args.experiment_name,
-      num_eval_episodes=args.num_eval_episodes,
-  )
+  app.run(main)
