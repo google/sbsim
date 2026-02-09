44c4754567f521e3190d675efb7577dae77fa1e9
Michael Rossetti <mjrossetti@google.com>
2026-02-09 21:21:31 +0000

    Weather controller sync-out

--- smart_control/simulator/weather_controller_test.py
+++ smart_control/simulator/weather_controller_test.py
@@ -121,42 +121,109 @@ class WeatherControllerTest(parameterized.TestCase):
 
     self.assertEqual(convection_coefficient, expected_convection_coefficient)
 
-  def test_replay_weather_controller(self):
 
+class ReplayWeatherControllerTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
     data_path = os.path.join(
         os.path.dirname(__file__), 'local_weather_test_data.csv'
     )
-    controller = weather_controller.ReplayWeatherController(data_path, 10.0)
-
-    temp = controller.get_current_temp(
-        pd.Timestamp('2023-07-01 03:00:01+00:00')
+    self.controller = weather_controller.ReplayWeatherController(
+        local_weather_path=data_path, convection_coefficient=10.0
     )
 
+  def test_replay_weather_controller(self):
+    temp = self.controller.get_current_temp(
+        pd.Timestamp('2023-07-01 03:00:01+00:00')
+    )
     self.assertAlmostEqual(temp, 298.1500, places=5)
 
   def test_replay_weather_controller_raises_error_before_range(self):
-    data_path = os.path.join(
-        os.path.dirname(__file__), 'local_weather_test_data.csv'
-    )
-    controller = weather_controller.ReplayWeatherController(data_path, 10.0)
-
-    weather_fn = lambda: controller.get_current_temp(
+    weather_fn = lambda: self.controller.get_current_temp(
         pd.Timestamp('2023-05-01 03:00:01+00:00')
     )
-
     self.assertRaises(ValueError, weather_fn)
 
   def test_replay_weather_controller_raises_error_after_range(self):
-    data_path = os.path.join(
-        os.path.dirname(__file__), 'local_weather_test_data.csv'
-    )
-    controller = weather_controller.ReplayWeatherController(data_path, 10.0)
-
-    weather_fn = lambda: controller.get_current_temp(
+    weather_fn = lambda: self.controller.get_current_temp(
         pd.Timestamp('2023-12-01 03:00:01+00:00')
     )
-
     self.assertRaises(ValueError, weather_fn)
+
+
+class MoffettReplayWeatherControllerTest(parameterized.TestCase):
+  """Tests for ReplayWeatherController using real weather data."""
+
+  def setUp(self):
+    super().setUp()
+    self.controller = weather_controller.ReplayWeatherController()
+
+  def test_weather_df(self):
+    self.assertIsInstance(self.controller.weather_df, pd.DataFrame)
+    self.assertEqual(self.controller.weather_df.shape, (3462, 15))
+
+    expected_columns = [
+        'Time',
+        'StationName',
+        'StationId',
+        'Location',
+        'TempC',
+        'DewPointC',
+        'BarometerMbar',
+        'Rain',
+        'RainTotal',
+        'WindspeedKmph',
+        'WindDirection',
+        'SkyCoverage',
+        'VisibilityKm',
+        'Humidity',
+        'TempF',
+    ]
+    self.assertCountEqual(
+        self.controller.weather_df.columns.tolist(),
+        expected_columns,
+    )
+
+  def test_time_range(self):
+    min_time = pd.Timestamp('2023-06-30 17:00:00+00:00')
+    max_time = pd.Timestamp('2023-11-22 16:00:00+00:00')
+
+    self.assertEqual(self.controller.min_time, min_time)
+    self.assertEqual(self.controller.max_time, max_time)
+
+  def test_times_in_seconds(self):
+    self.assertIsInstance(self.controller.times_in_seconds, pd.Index)
+    self.assertEqual(self.controller.times_in_seconds.shape, (3462,))
+
+    self.assertEqual(min(self.controller.times_in_seconds), 1688144400.0)
+    self.assertEqual(max(self.controller.times_in_seconds), 1700668800.0)
+
+  def test_get_temp_timezones(self):
+    with self.subTest('when timestamp is timezone aware'):
+      timestamp = pd.Timestamp('2023-07-01 10:00:00+00:00')
+      self.assertEqual(timestamp.tzname(), 'UTC')
+
+      temp = self.controller.get_current_temp(timestamp)
+      self.assertEqual(temp, 289.15)
+
+    with self.subTest('when timestamp is timezone naive'):
+      timestamp = pd.Timestamp('2023-07-01 10:00:00')
+      self.assertIsNone(timestamp.tzname())
+
+      temp = self.controller.get_current_temp(timestamp)
+      self.assertEqual(temp, 289.15)
+
+  def test_interpolation(self):
+    timestamp = pd.Timestamp('2023-07-01 03:00:01+00:00')
+
+    with self.subTest('current_temp'):
+      temp_k = self.controller.get_current_temp(timestamp)
+      self.assertAlmostEqual(temp_k, 294.1497, places=4)
+
+    with self.subTest('current_humidity'):
+      humidity = self.controller.get_current_humidity(timestamp)
+      self.assertAlmostEqual(humidity, 65.0, places=5)
 
 
 if __name__ == '__main__':
